0c051925966bb84e56905dcdddf51aae679aa69c
Logan <lflearns@gmail.com>
2019-03-06 13:49:46 -0700

    Finished update method for IncidentDB

--- ICT713-Lab3-LoganFidler/App_Code/IncidentDB.cs
+++ ICT713-Lab3-LoganFidler/App_Code/IncidentDB.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -14,27 +15,43 @@ namespace ICT713_Lab3_LoganFidler.App_Code
         [DataObjectMethod(DataObjectMethodType.Select)]
         public static IEnumerable GetTechIncidents(int TechID)
         {
-            SqlConnection con = new SqlConnection(TechSupportDB.GetConnectionString());
             string query = "SELECT IncidentID, DateOpened, "
                 + "Title, Description, CustomerID, ProductCode, DateClosed "
                 + "FROM Incidents WHERE TechID = @TechID "
                 + "ORDER BY DateOpened";
-            SqlCommand command = new SqlCommand(query, con);
+            SqlConnection connection = new SqlConnection(TechSupportDB.GetConnectionString());
+            SqlCommand command = new SqlCommand(query, connection);
             command.Parameters.AddWithValue("TechID", TechID);
-            con.Open();
+            connection.Open();
             SqlDataReader dataReader = command.ExecuteReader();
             return dataReader;
         }
+
+        
         [DataObjectMethod(DataObjectMethodType.Update)]
         public static int UpdateIncident(Incident original_Incident, Incident incident)
         {
-            int updateCount = 0;
+            int updateRow = 0;
             string query = "UPDATE Incidents "
                 + "SET DateClosed = @DateClosed, "
                 + "Description = @Description "
                 + "WHERE IncidentID = @original_IncidentID "
                 + "AND DateClosed = @original_DateClosed "
                 + "AND Description = @original_Description";
+            using (SqlConnection connection = new SqlConnection(TechSupportDB.GetConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("DateClosed", incident.DateClosed);
+                    command.Parameters.AddWithValue("Description", incident.Description);
+                    command.Parameters.AddWithValue("original_IncidentID", original_Incident.IncidentID);
+                    command.Parameters.AddWithValue("original_DateClosed", original_Incident.DateClosed);
+                    command.Parameters.AddWithValue("original_description", original_Incident.Description);
+                    connection.Open();
+                    updateRow = command.ExecuteNonQuery();
+                }
+            }
+            return updateRow;
         }
     }
 }
